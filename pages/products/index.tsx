0883d0950f4ed890c0fe77be99185ca9e59d7497
TheHaxy <vlad89896135637@mail.ru>
2022-06-27 03:38:47 +0300

    refactoring model and add Head

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Button } from '@mui/material'
 import { useStore } from 'effector-react'
+import Head from 'next/head'
 import { $productsStorage } from '../../model/model'
 import ItemsContainer from '../../components/ItemsContainer/ItemsContainer'
 import CreateProductModal from '../../components/products/CreateProductModal/CreateProductModal'
@@ -10,15 +11,21 @@ const Index = () => {
   const [modalIsOpened, setModalIsOpened] = useState(false)
 
   return (
-    <div>
-      {modalIsOpened && (
-        <CreateProductModal setModalIsOpened={setModalIsOpened} />
-      )}
-      <Button variant='outlined' onClick={() => setModalIsOpened(true)}>
-        Добавить продукт
-      </Button>
-      <ItemsContainer itemsStorage={productsStorage} />
-    </div>
+    <>
+      <Head>
+        <title>Products</title>
+        <meta charSet='utf-8' />
+      </Head>
+      <div>
+        {modalIsOpened && (
+          <CreateProductModal setModalIsOpened={setModalIsOpened} />
+        )}
+        <Button variant='outlined' onClick={() => setModalIsOpened(true)}>
+          Добавить продукт
+        </Button>
+        <ItemsContainer itemsStorage={productsStorage} />
+      </div>
+    </>
   )
 }
 
